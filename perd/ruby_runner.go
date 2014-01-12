8d0e22cbe40302832fc149ce7c6360003cbf88bc
Rozhnov Alexandr <nox73@ya.ru>
2014-01-12 23:08:25 +0400

    Docker rm

--- perd/ruby_runner.go
+++ perd/ruby_runner.go
@@ -20,10 +20,11 @@ func (r *RubyRunner) Run () {
 
       ioutil.WriteFile("/tmp/1.rb", []byte(c.Command()), 755)
 
-      out, err := exec.Command("docker", "run -v /tmp:/tmp/host -name=\"ruby\" fd61e37b54de bash -l -c \"ruby /tmp/host/1.rb\";").Output()
+      out, err := exec.Command("docker", "run -v /tmp:/tmp/host -name=\"ruby\" fd61e37b54de bash -l -c \"ruby /tmp/host/1.rb\"").Output()
       if err != nil { log.Println(err) }
 
       c.Response(string(out))
+      exec.Command("docker", "rm ruby")
     }
 
   }()
