ed2802c1af4b88cae9821ee8fd564848639b331d
Rozhnov Alexandr <nox73@ya.ru>
2014-01-16 00:19:29 +0400

    publick result fields.

--- perd/result.go
+++ perd/result.go
@@ -11,9 +11,9 @@ func NewResult(out, err []byte, code int) Result {
 }
 
 type result struct {
-  stdOut      string `json:"std_out"`
-  stdErr      string `json:"std_err"`
-  statusCode  int `json:"code"`
+  StdOut      string `json:"std_out"`
+  StdErr      string `json:"std_err"`
+  StatusCode  int `json:"code"`
 }
 
 func (r *result) Bytes () []byte {
