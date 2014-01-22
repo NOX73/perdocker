b0eb910dcca9fe1276578a24d007ec91c5dfbd5d
Rozhnov Alexandr <nox73@ya.ru>
2014-01-22 23:39:26 +0400

    New worker.

--- perd/exec.go
+++ perd/exec.go
@@ -8,6 +8,7 @@ import (
 
 var (
   ErrExecTimout = errors.New("Execution timeout.")
+  ErrReadStds = errors.New("Read std terminated.")
 )
 
 type Exec struct {
@@ -16,7 +17,7 @@ type Exec struct {
 
   end []byte
 
-  done chan bool
+  done chan error
 
   StdOut []byte
   StdErr []byte
@@ -34,8 +35,8 @@ func NewExec (out, err <-chan []byte, end []byte) *Exec {
 
 func (e *Exec) Wait(timeout time.Duration) error {
   select {
-  case <-e.done:
-    return nil
+  case err <-e.done:
+    return err
   case <-time.After(timeout):
     return ErrExecTimout;
   }
@@ -47,23 +48,31 @@ func (e *Exec) Start () {
     if e.isFinish() {break}
 
     select {
-    case line := <- e.out:
+    case line, ok := <- e.out:
+
+      if !ok { e.done <- ErrReadStds; return }
+
       if e.isEnd(line) {
         e.out = nil
         e.ExitCode = e.extractCode(line)
       } else {
         e.StdOut = append(e.StdOut, line...)
       }
-    case line := <- e.err:
+
+    case line, ok := <- e.err:
+
+      if !ok { e.done <- ErrReadStds; return }
+
       if e.isEnd(line) {
         e.err = nil
       } else {
         e.StdErr = append(e.StdErr, line...)
       }
+
     }
   }
 
-  e.done <- true
+  e.done <- nil
 }
 
 func (e *Exec) extractCode (line []byte) int {
