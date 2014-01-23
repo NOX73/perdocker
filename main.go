91471d033dac2c243f295708127263020e54ed2a
Anton Kalyaev <anton.kalyaev@gmail.com>
2014-01-23 16:14:34 +0400

    golang support

--- main.go
+++ main.go
@@ -9,7 +9,10 @@ import (
 var httpPort string
 
 func main() {
-	var rubyWorkers, nodejsWorkers int
+	var rubyWorkers int
+	var nodejsWorkers int
+	var golangWorkers int
+
 	var timeout int64
 	var w sync.WaitGroup
 	w.Add(1)
@@ -17,10 +20,11 @@ func main() {
 	flag.StringVar(&httpPort, "port", "8080", "HTTP server port.")
 	flag.IntVar(&rubyWorkers, "ruby-workers", 1, "Count of ruby workers.")
 	flag.IntVar(&nodejsWorkers, "nodejs-workers", 1, "Count of nodejs workers.")
+	flag.IntVar(&golangWorkers, "golang-workers", 1, "Count of go workers.")
 	flag.Int64Var(&timeout, "timeout", 30, "Max execution time.")
 	flag.Parse()
 
-	workers := map[string]int{"ruby": rubyWorkers, "nodejs": nodejsWorkers}
+	workers := map[string]int{"ruby": rubyWorkers, "nodejs": nodejsWorkers, "golang": golangWorkers}
 	server := perd.NewServer(httpPort, workers, timeout)
 
 	go func() {
--- perd/server.go
+++ perd/server.go
@@ -15,14 +15,16 @@ type config struct {
 func NewServer(port string, workers map[string]int, timeout int64) Server {
 	ruby := NewRunner(Ruby, workers["ruby"], timeout)
 	nodejs := NewRunner(Nodejs, workers["nodejs"], timeout)
+	golang := NewRunner(Golang, workers["golang"], timeout)
 
-	return &server{&config{port}, ruby, nodejs}
+	return &server{&config{port}, ruby, nodejs, golang}
 }
 
 type server struct {
 	config       *config
 	rubyRunner   Runner
 	nodejsRunner Runner
+	golangRunner Runner
 }
 
 func (s *server) Run() {
@@ -30,6 +32,7 @@ func (s *server) Run() {
 
 	http.HandleFunc("/ruby", s.rubyHandler)
 	http.HandleFunc("/nodejs", s.nodejsHandler)
+	http.HandleFunc("/go", s.golangHandler)
 
 	log.Println("Listen http on", s.config.port)
 	http.ListenAndServe(":"+s.config.port, nil)
@@ -50,6 +53,16 @@ func (s *server) rubyHandler(w http.ResponseWriter, r *http.Request) {
 
 	if err == nil {
 		res := s.rubyRunner.Eval(string(body))
+		w.Write(res.Bytes())
+	}
+
+}
+
+func (s *server) golangHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err == nil {
+		res := s.golangRunner.Eval(string(body))
 		w.Write(res.Bytes())
 	}
 
