656550e6a9b528db8a3313513f400fa26d23856d
Anton Kalyaev <anton.kalyaev@gmail.com>
2014-01-23 13:40:41 +0400

    formatted code using fmt

--- perd/exec.go
+++ perd/exec.go
@@ -1,89 +1,99 @@
 package perd
 
 import (
-  "strconv"
-  "time"
-  "errors"
+	"errors"
+	"strconv"
+	"time"
 )
 
 var (
-  ErrExecTimout = errors.New("Execution timeout.")
-  ErrReadStds = errors.New("Read std terminated.")
+	ErrExecTimout = errors.New("Execution timeout.")
+	ErrReadStds   = errors.New("Read std terminated.")
 )
 
 type Exec struct {
-  out <-chan []byte
-  err <-chan []byte
+	out <-chan []byte
+	err <-chan []byte
 
-  end []byte
+	end []byte
 
-  done chan error
+	done chan error
 
-  StdOut []byte
-  StdErr []byte
-  ExitCode int
+	StdOut   []byte
+	StdErr   []byte
+	ExitCode int
 }
 
-func NewExec (out, err <-chan []byte, end []byte) *Exec {
-  return &Exec {
-    out: out,
-    err: err,
-    end: end,
-    done: make(chan error, 1),
-  }
+func NewExec(out, err <-chan []byte, end []byte) *Exec {
+	return &Exec{
+		out:  out,
+		err:  err,
+		end:  end,
+		done: make(chan error, 1),
+	}
 }
 
 func (e *Exec) Wait(timeout time.Duration) error {
-  select {
-  case err := <-e.done:
-    return err
-  case <-time.After(timeout):
-    return ErrExecTimout;
-  }
+	select {
+	case err := <-e.done:
+		return err
+	case <-time.After(timeout):
+		return ErrExecTimout
+	}
 }
 
-func (e *Exec) Start () {
+func (e *Exec) Start() {
 
-  for {
-    if e.isFinish() {break}
+	for {
+		if e.isFinish() {
+			break
+		}
 
-    select {
-    case line, ok := <- e.out:
-      if !ok { e.done <- ErrReadStds; return }
+		select {
+		case line, ok := <-e.out:
+			if !ok {
+				e.done <- ErrReadStds
+				return
+			}
 
-      if e.isEnd(line) {
-        e.out = nil
-        e.ExitCode = e.extractCode(line)
-      } else {
-        e.StdOut = append(e.StdOut, line...)
-      }
+			if e.isEnd(line) {
+				e.out = nil
+				e.ExitCode = e.extractCode(line)
+			} else {
+				e.StdOut = append(e.StdOut, line...)
+			}
 
-    case line, ok := <- e.err:
-      if !ok { e.done <- ErrReadStds; return }
+		case line, ok := <-e.err:
+			if !ok {
+				e.done <- ErrReadStds
+				return
+			}
 
-      if e.isEnd(line) {
-        e.err = nil
-      } else {
-        e.StdErr = append(e.StdErr, line...)
-      }
+			if e.isEnd(line) {
+				e.err = nil
+			} else {
+				e.StdErr = append(e.StdErr, line...)
+			}
 
-    }
-  }
+		}
+	}
 
-  e.done <- nil
+	e.done <- nil
 }
 
-func (e *Exec) extractCode (line []byte) int {
-  scode := string(line)[len(e.end) : len(line)-1]
-  code, err := strconv.Atoi(scode)
-  if err != nil { return 1 }
-  return code
+func (e *Exec) extractCode(line []byte) int {
+	scode := string(line)[len(e.end) : len(line)-1]
+	code, err := strconv.Atoi(scode)
+	if err != nil {
+		return 1
+	}
+	return code
 }
 
-func (e *Exec) isEnd (line []byte) bool {
-  return len(line) > len(e.end) && string(line[:len(e.end)]) == string(e.end)
+func (e *Exec) isEnd(line []byte) bool {
+	return len(line) > len(e.end) && string(line[:len(e.end)]) == string(e.end)
 }
 
-func (e *Exec) isFinish () bool {
-  return e.out == nil && e.err == nil
+func (e *Exec) isFinish() bool {
+	return e.out == nil && e.err == nil
 }
