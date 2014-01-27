3178fb02793962b5c9038c43b8fd8c98e4e19e5b
Rozhnov Alexandr <nox73@ya.ru>
2014-01-27 22:34:31 +0400

    Added cli flag c-workers.

--- main.go
+++ main.go
@@ -11,13 +11,14 @@ var rubyWorkers = flag.Int64("ruby-workers", 1, "Count of ruby workers.")
 var nodejsWorkers = flag.Int64("nodejs-workers", 1, "Count of nodejs workers.")
 var golangWorkers = flag.Int64("golang-workers", 1, "Count of golang workers.")
 var pythonWorkers = flag.Int64("python-workers", 1, "Count of python workers.")
+var cWorkers = flag.Int64("c-workers", 1, "Count of C workers.")
 
 var timeout = flag.Int64("timeout", 30, "Max execution time.")
 
 func main() {
 	flag.Parse()
 
-	workers := map[string]int64{"ruby": *rubyWorkers, "nodejs": *nodejsWorkers, "golang": *golangWorkers, "python": *pythonWorkers}
+	workers := map[string]int64{"ruby": *rubyWorkers, "nodejs": *nodejsWorkers, "golang": *golangWorkers, "python": *pythonWorkers, "c": *cWorkers}
 	server := perd.NewServer(*httpListen, workers, *timeout)
 
 	server.Run()
